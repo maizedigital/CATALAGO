73e91997fe2e0f30a3dbc033571f01d65016f3db
maizedigital <agencia.maizedigital@gmail.com>
2026-09-03 16:56:46 -0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
 import { useEffect } from 'react';
 
 
-
-
-
-
-
-
 const removeFloating = () => {
 
   document.querySelectorAll('[style="position: fixed"][style="bottom: 1rem"][style="right: 1rem"][style="z-index: 2147483647"]').forEach(el => el.remove());
 
 };
-
-
-
 // executa já no load
 
 removeFloating();
-
-
-
 // observa mudanças no DOM
 
 const observer = new MutationObserver(removeFloating);
