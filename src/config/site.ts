2ddc8bfd24f0fb1eef6df11a36f24a342d2b46a0
maizedigital <agencia.maizedigital@gmail.com>
2026-09-14 11:17:54 -0300

    Updated site.ts

--- src/config/site.ts
+++ src/config/site.ts
@@ -10,7 +10,7 @@ export const siteConfig = {
   instagram: '@mbmodabrasil',
   instagramUrl: 'https://instagram.com/mbmodabrasil',
   address: 'BR-367, km 77 — Coroa Vermelha, Santa Cruz Cabrália — BA, 45810-000',
-  hoursStore: 'Segunda a sábado: 08:30 às 18:30',
+  hoursStore: 'Segunda a sábado: 08:30 às 18:10',
   hoursSite: 'Disponível 24 horas por dia, 7 dias por semana',
 };
 
